8e18bc38a39e50ca3b9619fc3274d8375c401041
Carsten Dormann <carsten.dormann@biom.uni-freiburg.de>
2021-01-19 18:49:54 +0100

    CRAN release build; static PDF replaces dynamic but VERY slow vignette; after about 40 winbuilder checks I have sorted out all issues introduced by roxygen (?)

--- tapnet_packaging.R
+++ tapnet_packaging.R
@@ -1,27 +1,60 @@
+
+## testtest: start with yesterdays tapnet, remove vignette, re-build and re-submit to winbuilder:
+fine!
+## changed description: no author, no maintainer, suggest only knitr, vignetteBuilder knit and utils
+fine!
+## Re-run roxygen ("document") on vignette-free package: does it screw it up?
+fine!
+## Add vignette folder, roxygenise
+!! error message about LOCK-file in deleted temp-folder (private/var/folders/...); Re-installed R, no effect
+## copied all old R and Description function from yesterday onto current folder; re-submit to winbuilder
+fine! (except for pdf compaction)
+## added --compact-vignettes=gs+qpdf to tapnet-mainline, resubmit to release
+fine! (except for PDF compaction)
+## fixed error (testtest; deleted "build" folder), build package, submitted to develop! 
+fine!
+## mainline tapnet build with devtools and uploaded to release with two text snippets added to tapnet-package (e.g. reference)
+fine!
+## same mainline/devtools to devel:
+
+  
+# ideen: 
+* utils in description?
+* LF line endings corrupted (why?)
+* roxygen screwed up
+
+## winbuilder screws up: cache issues? --> new try on old-release with devtoolsbuild; no, same problem there
+## removed vignette (in testtest) and uploaded to release: same problem
+## release: packaged with devtools --> error:
+** R
+Error in parse(outFile) : 
+  d:/temp/Rtmp0eBoln/R.INSTALL102386d2e5c71/tapnet/R/tapnet-package.R:1:1: unexpected '<'
+1: <
+  ^
+  ERROR: unable to collate and parse R files for package 'tapnet'
+## devel: packaged on ubuntu, command line: same error
+
+
+
 library(devtools)
 setwd("~/Data/aktuell/Networks/tapnet/tapnet")
 document() # process R-functions into .RD files, change namespace
-setwd("..")
-build("tapnet", args="--compact-vignettes")   # build the .tar.gz file  ## takes 11 min because of the processing of the .Rnw file!!
-#build("tapnet", args="--no-build-vignettes", clean_doc=FALSE) # if vignette hasn't changed; does not work for package to be uploaded to CRAN!!
-install("tapnet") # install on the computer
-# check_built("tapnet") # is included in:
-devtools::check("tapnet")
+#tools::compactPDF("vignettes/", gs_quality = "ebook")
 
-# hm; I get an error message from winbuilder with this, saying: 
-* checking index information ... WARNING
-Vignettes with missing or empty \VignetteIndexEntry:
-  CaseStudy.Rnw
-See sections 'The INDEX file' and 'Package subdirectories' in the
-'Writing R Extensions' manual.
-# And indeed, the installed tapnet package html has no link to the CaseStudy. 
-# When using the command line, the link is there ...
-R CMD build tapnet --compact-vignettes
+## Starting from here, the performance seems to vary:
+setwd("..")
+build("tapnet", args="--compact-vignettes=gs+qpdf", binary=F) 
+#install("tapnet") # install on the computer
+devtools::check("tapnet", args="--as-cran")
+# I don't get why it returns a warning about compactable PDF: it IS compacted and there is nothing anyone can do about this in the build!
+
+## Using winbuilder (https://win-builder.r-project.org/upload.aspx) to check before submission, I repeatedly get funny problems when using the devtools-developed package. I suspect that some funny temporary file is involved, as the local errors point to var/local/cc/temp...
+## Command line building works (mostly) fine.
+R CMD build tapnet --compact-vignettes=gs+qpdf
 R CMD check tapnet_0.3.tar.gz --as-cran
 
-
-
-
+# %%%%%%%%%%%%%%%%%%%%%%%%%     %%%%%%%%%%%%%%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%     %%%%%%%%%%%%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%     %%%%%%%%%%%%%%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%     %%%%%%%%%%%%%%%%%%%%%%%%%
 # prepare things:
 #save(humm_traits, humm_tree, networks, plant_traits, plant_tree, file="data/Tinoco.rda")
 
@@ -49,3 +82,4 @@ str(fit)
 
 # wishlist:
 * add correlation method option to gof (not only Spearman)
+* add deOptim as optimiser (more robust but still fast, I hope)
